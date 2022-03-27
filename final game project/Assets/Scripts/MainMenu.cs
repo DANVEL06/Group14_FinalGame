82adc4bf0171d47b7b575789429ca4496b6a014a
danieldra910 <daniel_arias2000@knights.ucf.edu>
2022-03-27 18:52:07 -0400

    incorporate main menu

---
+++ final game project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void HowToPlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
