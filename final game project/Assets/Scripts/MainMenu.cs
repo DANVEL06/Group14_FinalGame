b79d4820349c30d8580b774d6146b91bbcf02cdf
svmmy416 <svmmy24@knights.ucf.edu>
2022-03-27 16:40:46 -0400

    Menu UI updates
    fixed a problem i had with layering by making a new scene for how to play

--- final game project/Assets/Scripts/MainMenu.cs
+++ final game project/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,17 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        SceneManager.LoadScene(2);
+    }
+
+    public void HowToPlayGame()
+    {
         SceneManager.LoadScene(1);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void QuitGame()
