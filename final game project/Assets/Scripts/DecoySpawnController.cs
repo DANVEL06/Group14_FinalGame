716f10c38b4881b3296c5ebbb7c8477f4a8eca56
danieldra910 <daniel_arias2000@knights.ucf.edu>
2022-04-16 21:36:03 -0400

    failed attempt to improve decoy

--- final game project/Assets/Scripts/DecoySpawnController.cs
+++ final game project/Assets/Scripts/DecoySpawnController.cs
@@ -13,24 +13,40 @@ public class DecoySpawnController : MonoBehaviour
     private float delay;
 
      public AudioSource decoySpawn;
+     public bool canSpawn;
     // Start is called before the first frame update
     void Start()
     {
         
         spawned = false;
         delay =5;
+        //canSpawn = true;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(delay >=5 && Input.GetKey(KeyCode.LeftControl))
+        if(delay >=5  && Input.GetKey(KeyCode.LeftControl))
         {
-            decoySpawn.Play();
+           decoySpawn.Play();
            Instantiate (decoy, spawnPos.position, Quaternion.identity );
            delay = 0;
         }
         delay += Time.deltaTime;
     }
+    void OnCollisionEnter(Collision other)
+    {
+        
+            if(other.gameObject.tag == "Obstacle")
+            {
+                canSpawn = false;
+            }
+            else
+            {
+                canSpawn = true;
+            }
+        
+    }
+    
 }
